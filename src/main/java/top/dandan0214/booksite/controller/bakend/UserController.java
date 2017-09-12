d8a4642fefe0bb84d4c01a20de41dc10ee33c177
gang.yang <gang.yang@maiunsoft.com>
2017-09-12 16:35:23 +0800

    测试jenkins自动构建

--- src/main/java/top/dandan0214/booksite/controller/bakend/UserController.java
+++ src/main/java/top/dandan0214/booksite/controller/bakend/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
 	@RequestMapping("/login")
 	public ResponseModel login(String username, String password, Integer type) {
 		if (null == type) {
-			return ResponseModel.error("登录失败，失败原因：不识别的身份");
+			return ResponseModel.error("登录失败，失败原因：不识别的身份。");
 		}
 		if (null == username || username.trim().equals("")) {
 			return ResponseModel.error("登录失败，失败原因：用户名为空");
